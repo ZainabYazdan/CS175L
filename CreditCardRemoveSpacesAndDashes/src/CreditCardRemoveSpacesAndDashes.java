2bcf9f2aff827f4d8ad8d637f3284f190fe5322e
Vaniya <Vaniya@DESKTOP-0CF7SOV>
2021-04-11 16:54:36 +0500

    Credit card

---
+++ CreditCardRemoveSpacesAndDashes/src/CreditCardRemoveSpacesAndDashes.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+public class CreditCardRemoveSpacesAndDashes {
+
+	public static void main(String[] args) {
+		
+        Scanner in = new Scanner(System.in);
+        
+		int i = 0;
+		String creditCardNumber = "";
+		while (!creditCardNumber.equals("-99"))
+		
+		{
+			System.out.print("Enter a Credit Card Number or -99 to end: ");
+			creditCardNumber = in.nextLine();
+			while (i < creditCardNumber.length() && !creditCardNumber.equals("-99"))
+		  {
+				char ch = creditCardNumber.charAt(i);
+				if (ch == ' ' || ch == '-')
+		          {
+					String before = creditCardNumber.substring(0, i);
+					String after = creditCardNumber.substring(i + 1);
+					creditCardNumber = before + after;
+		          }
+				else
+				  {
+					i++;
+				  }
+		  }
+			System.out.println(creditCardNumber);
+			i = 0;
+		}
+		
+		System.out.println("Goodbye!");
+		
+		
+
+	}
+
+}
