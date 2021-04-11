cd984160847208eb5ca606562769b96ac0f0340b
Vaniya <Vaniya@DESKTOP-0CF7SOV>
2021-04-11 17:25:39 +0500

    multiplicationTable

---
+++ multiplicationTable/src/multiplicationTable/multiplicationTable.java
@@ -0,0 +1,21 @@
+package multiplicationTable;
+
+public class multiplicationTable {
+
+	public static void main(String[] args) {
+		
+	       for(int a=1;a<=10;a++)
+	       {
+	    	   for(int x=1; x<=10; x++)
+	    	   {
+	    		   System.out.printf("%5d",a*x);
+	    	   }
+	    	   System.out.println();
+	       }
+		}
+
+	
+
+	}
+
+
